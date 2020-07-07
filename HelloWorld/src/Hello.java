62aec83b6c9e451d6d8dcfdf76353fb31a81ff05
Luke Vandekieft <vandekie@gmail.com>
2020-07-07 13:39:15 -0700

    Complete HelloWorld function.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
 
   public static void main(String[] args) {
-  System.out.println("Hello Luke");
+  System.out.println("Hello Luke"); // console.log() equivalent, intelliJ autocompletes this with sout
+
+  int myFirstNumber = 5 + 5 + (2 * 10);
+  int mySecondNumber = 17; // greyed out, not used
+    System.out.println(myFirstNumber);
   }
 }
