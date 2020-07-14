d017cd9665797ba4f5bdd018ac97a526f00fc8c2
lukevandekieft <vandekie@gmail.com>
2020-07-14 12:03:52 -0700

    Add function that returns value.

--- Functions/src/com/company/Functions.java
+++ Functions/src/com/company/Functions.java
@@ -4,6 +4,7 @@ public class Functions {
 
     public static void main(String[] args) {
         calculateScore(true, 100, 120);
+        System.out.println(calculateAndReturnScore(true, 300, 60));
     }
 
     public static void calculateScore(boolean gameOver, int score, int bonus) {
@@ -12,6 +13,14 @@ public class Functions {
             System.out.println(finalScore);
         } else {
             System.out.println("Game not over!");
+        }
+    }
+
+    public static int calculateAndReturnScore(boolean gameOver, int score, int bonus) {
+        if(gameOver) {
+            return score + bonus;
+        } else {
+            return -1;
         }
     }
 }
