758da0931b39e278eddea7c62fce7c6df020fa59
lukevandekieft <vandekie@gmail.com>
2020-07-14 14:41:03 -0700

    Add overloaded function example.

--- Functions/src/com/company/Functions.java
+++ Functions/src/com/company/Functions.java
@@ -4,21 +4,32 @@ public class Functions {
 
     public static void main(String[] args) {
         calculateScore(true, 100, 120);
-        System.out.println(calculateAndReturnScore(true, 300, 60));
+        System.out.println("Function return result: " + calculateAndReturnScore(true, 300, 60));
+        System.out.println("Overloaded function result: " + calculateAndReturnScore(true, 300));
     }
 
+    // void = no return value
     public static void calculateScore(boolean gameOver, int score, int bonus) {
         if(gameOver) {
             int finalScore = score + bonus;
-            System.out.println(finalScore);
+            System.out.println("Printed call score: " + finalScore);
         } else {
             System.out.println("Game not over!");
         }
     }
 
+    // int = return value int
     public static int calculateAndReturnScore(boolean gameOver, int score, int bonus) {
         if(gameOver) {
             return score + bonus;
+        } else {
+            return -1;
+        }
+    }
+
+    public static int calculateAndReturnScore(boolean gameOver, int score) {
+        if(gameOver) {
+            return score;
         } else {
             return -1;
         }
