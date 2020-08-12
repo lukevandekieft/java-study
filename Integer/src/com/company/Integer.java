56584edb10f17379e15ba1251e09249cde8302fb
lukevandekieft <vandekie@gmail.com>
2020-08-12 15:12:26 -0700

    '--no-commit' flag allowed me to copy change #1 w/o committing.

--- Integer/src/com/company/Integer.java
+++ Integer/src/com/company/Integer.java
@@ -3,7 +3,7 @@ package com.company;
 public class Integer {
 
     public static void main(String[] args) {
-        String numberAsString = "2018";
+        String numberAsString = "2020";
         System.out.println("number As String = " + numberAsString);
     }
 }
