fbc146e961b7f64629a22cafe97a08e286d2ac77
lukevandekieft <vandekie@gmail.com>
2020-08-12 15:07:55 -0700

    Cherry pick commit #2: change 'hello Luke' to 'hello World'.

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
   public static void main(String[] args) {
-  System.out.println("Hello Luke"); // console.log() equivalent, intelliJ autocompletes this with sout
+  System.out.println("Hello World"); // console.log() equivalent, intelliJ autocompletes this with sout
 
   int myFirstNumber = 5 + 5 + (2 * 10);
   int mySecondNumber = 17; // greyed out, not used
