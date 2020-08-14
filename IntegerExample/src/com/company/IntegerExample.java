f847e5109255e305dcbd0916611c32f9f9698e13
lukevandekieft <vandekie@gmail.com>
2020-08-14 13:10:24 -0700

    Add integer conversion cases.

--- IntegerExample/src/com/company/IntegerExample.java
+++ IntegerExample/src/com/company/IntegerExample.java
@@ -7,6 +7,16 @@ public class IntegerExample {
         System.out.println("number As String = " + numberAsString);
 
         int number = Integer.parseInt(numberAsString);
+//        int number = Integer.parseInt("20ee9")  <-- Passing non-int to parseInt is a breaking error!
         System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println(numberAsString);
+        System.out.println(number);
+
+        double doubleNumber = Double.parseDouble(numberAsString);   // <-- Must be String, not int!
+        System.out.println(doubleNumber);
     }
 }
