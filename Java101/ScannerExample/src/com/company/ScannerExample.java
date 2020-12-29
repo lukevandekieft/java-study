91471347d42b628df724f92cd473869cb6d49415
lukevandekieft <vandekie@gmail.com>
2020-12-29 14:48:10 -0800

    Add conditions to scanner input.

--- Java101/ScannerExample/src/com/company/ScannerExample.java
+++ Java101/ScannerExample/src/com/company/ScannerExample.java
@@ -10,11 +10,23 @@ public class ScannerExample {
         String name = scanner.nextLine(); // <-- this stops code execution and waits for console input
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        int age = 2020 - yearOfBirth;
-//        scanner.nextLine();  <-- when using nextInt and you expect further input you should include an extra nextLine to accout for "Enter" being pressed
 
-        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            // scanner.nextLine();  <-- when using nextInt and you expect further input you should include an extra nextLine to account for "Enter" being pressed
+            int age = 2020 - yearOfBirth;
+
+            if(age >= 0 && age <= 120) {
+                System.out.println("Your name is " + name + " and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth.");
+            }
+        } else {
+            System.out.println("Invalid date provided.");
+        }
+
         scanner.close();
     }
 }
