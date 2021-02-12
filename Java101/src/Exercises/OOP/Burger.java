1aef74c105686efd879dace9791c1301d1eac318
lukevandekieft <vandekie@gmail.com>
2021-02-12 15:44:01 -0800

    Update to match solution.

--- Java101/src/Exercises/OOP/Burger.java
+++ Java101/src/Exercises/OOP/Burger.java
@@ -5,8 +5,8 @@ import java.math.BigDecimal;
 public class Burger {
   private String roll;
   private String protein;
-  private String name = "Basic Burger";
-  private int basePrice = 299;
+  private String name;
+  private int basePrice;
   private boolean onions = false;
   private int onionsPrice = 45;
   private boolean cheese = false;
@@ -16,24 +16,42 @@ public class Burger {
 
   public String getPrice () {
     double price = basePrice;
+    String additions = "";
     if (onions) {
       price += onionsPrice;
+      additions += "Onions added. ";
     }
     if (cheese) {
       price += cheesePrice;
+      additions += "Cheese added. ";
     }
     if (avocado) {
       price += avocadoPrice;
+      additions += "Avocado added. ";
     }
-    return "Current price is " + (price/100);
+    return "Current price is " + (price/100) + ". " + additions;
+  }
+
+  public void addAvocado() {
+    avocado = true;
+  }
+
+  public void addCheese() {
+    cheese = true;
+  }
+
+  public void addOnions() {
+    onions = true;
   }
 
   public String getFullPrice () {
     return "Base price is: " + (double) basePrice/100 + ". Avocado price is: " + (double) avocadoPrice/100 + ". Price for fully loaded burger is: " + (double) (basePrice + onionsPrice + cheesePrice + avocadoPrice)/100 + ".";
   }
 
-  public Burger(String roll, String protein) {
+  public Burger(String roll, String protein, String name, int price) {
       this.roll = roll;
       this.protein = protein;
+      this.name = name;
+      this.basePrice = price;
   }
 }
--- Java101/src/Exercises/OOP/Main.java
+++ Java101/src/Exercises/OOP/Main.java
@@ -2,9 +2,11 @@ package Exercises.OOP;
 
 public class Main {
   public static void main(String[] args) {
-    Burger basicBurger = new Burger("Wheat", "Beyond");
-    String price = basicBurger.getPrice();
-    System.out.println(price);
+    Burger basicBurger = new Burger("Wheat", "Beyond", "Beyond Burger", 499);
+    System.out.println(basicBurger.getPrice());
+    basicBurger.addCheese();
+    basicBurger.addAvocado();
+    System.out.println(basicBurger.getPrice());
     System.out.println(basicBurger.getFullPrice());
   }
 }
